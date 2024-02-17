57b4f254afa1c5ee9fc21ce238d00b65d71af73e
Алексей <alexjokerr93@gmail.com>
2024-02-17 18:21:09 +0300

    Initial commit

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from sqlalchemy import or_
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:admin@localhost/gym'  # Замените username, password и db_name на ваши данные
@@ -13,6 +15,18 @@ class Clients(db.Model):
     last_name = db.Column(db.String(50))
     birth_date = db.Column(db.Date)
     gender = db.Column(db.Enum('Мужской', 'Женский'))
+    sub_type = db.Column(db.String(50))
+    sub_expiry = db.Column(db.Date)
+    phone_number = db.Column(db.String(15))
+
+    @classmethod
+    def create(cls, first_name, last_name, birth_date, gender, sub_type, sub_expiry):
+        client = cls(first_name=first_name, last_name=last_name, birth_date=birth_date, gender=gender,
+                     sub_type=sub_type, sub_expiry=sub_expiry)
+        db.session.add(client)
+        db.session.commit()
+        return client
+
 
 class Equipment(db.Model):
     __tablename__ = 'equipment'
@@ -61,8 +75,12 @@ def index():
 # Маршрут для отображения клиентов
 @app.route('/clients')
 def clients():
+    return render_template('clients.html')
+
+@app.route('/clients_list')
+def clients_list():
     clients = Clients.query.all()  # Извлекаем всех клиентов из базы данных
-    return render_template('clients.html', clients=clients)
+    return render_template('clients_list.html', clients=clients)
 
 # Маршрут для отображения оборудования
 @app.route('/equipment')
@@ -96,13 +114,45 @@ def workouts():
 
 @app.route('/search')
 def search():
-    query = request.args.get('query')  # Получаем запрос из параметра запроса GET
-    # Выполните поиск клиентов по имени в вашей базе данных или списке клиентов
-    # Например, если у вас есть список клиентов clients, выполните фильтрацию по имени
-    clients = Clients.query.all()
-    search_results = [client for client in clients if query.lower() in client.first_name.lower() or query.lower() in client.last_name.lower()]
-    # Отобразите результаты поиска в вашем шаблоне
-    return render_template('clients.html', search_results=search_results, query=query)
+    query = request.args.get('query')
+
+    if query:
+        # Используем метод filter() для фильтрации записей по нескольким полям с помощью логического оператора ИЛИ (OR)
+        search_results = Clients.query.filter(
+            or_(
+                Clients.first_name.ilike(f'%{query}%'),  # Ищем по имени (игнорируя регистр)
+                Clients.last_name.ilike(f'%{query}%'),   # Ищем по фамилии (игнорируя регистр)
+                Clients.sub_type.ilike(f'%{query}%'),    # Ищем по типу подписки (игнорируя регистр)
+                Clients.phone_number.ilike(f'%{query}%') # Ищем по номеру телефона (игнорируя регистр)
+            )
+        ).all()
+    else:
+        search_results = []
+
+    return render_template('client_search.html', search_results=search_results, query=query)
+
+
+
+
+@app.route('/add_client', methods=['GET', 'POST'])
+def add_client():
+    if request.method == 'POST':
+        # Получение данных из формы
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        birth_date = datetime.strptime(request.form['birth_date'], '%Y-%m-%d')
+        gender = request.form['gender']
+        sub_type = request.form['sub_type']
+        sub_expiry = datetime.strptime(request.form['sub_expiry'], '%Y-%m-%d')
+
+        # Создание нового клиента
+        client = Clients(first_name=first_name, last_name=last_name, birth_date=birth_date, gender=gender, sub_type=sub_type, sub_expiry=sub_expiry)
+        db.session.add(client)
+        db.session.commit()
+
+        return redirect(url_for('index'))  # Перенаправление на главную страницу
+
+    return render_template('add_client.html')
 
 
 
