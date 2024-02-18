48ec156351435131ce56044d6992c6b1dd73ace2
Алексей <alexjokerr93@gmail.com>
2024-02-18 14:47:36 +0300

    Initial commit

---
+++ static/common.js
@@ -0,0 +1,22 @@
+// Ожидаем, пока загрузится вся страница
+$(document).ready(function() {
+    // Обработчик события отправки формы
+    $(".search-form").submit(function(event) {
+        // Предотвращаем стандартное поведение браузера (перезагрузку страницы)
+        event.preventDefault();
+
+        // Получаем данные формы
+        var formData = $(this).serialize();
+
+        // Отправляем AJAX-запрос
+        $.ajax({
+            type: "GET", // Метод запроса
+            url: $(this).attr("action"), // URL для отправки запроса
+            data: formData, // Данные формы
+            success: function(response) {
+                // Обновляем содержимое страницы
+                $("#content").html(response);
+            }
+        });
+    });
+});
